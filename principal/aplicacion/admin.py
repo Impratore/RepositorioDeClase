2dae8156ad0c57e9e94ae2029531a5ab309d03c6
Impratore <oberkraucen@gmail.com>
2024-07-02 21:39:23 -0400

    nada

--- principal/aplicacion/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Category, Product, Cliente, Pedido, Reseña
-
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Cliente)
-admin.site.register(Pedido)
-admin.site.register(Reseña)
